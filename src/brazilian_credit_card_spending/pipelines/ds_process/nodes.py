92988d99a0f8749bb73b9bf40ef55828e8844dd8
richard <richard@truepay.com.br>
2024-04-02 23:19:54 -0400

    feat: HistGradientBoostingClassifier with results

--- src/brazilian_credit_card_spending/pipelines/ds_process/nodes.py
+++ src/brazilian_credit_card_spending/pipelines/ds_process/nodes.py
@@ -3,11 +3,14 @@ This is a boilerplate pipeline 'ds_process'
 generated using Kedro 0.19.3
 """
 
-
-from typing import Dict, List
+import logging
+from typing import Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 from thefuzz import fuzz
 
 
@@ -59,7 +62,75 @@ def _labels(df: pd.DataFrame, expense_groups: Dict[str, List[str]]) -> pd.DataFr
 def get_model_data(
     df: pd.DataFrame, expense_groups: Dict[str, List[str]]
 ) -> pd.DataFrame:
+    """Prepare data for model training.
+
+    Args:
+        df: Data containing features and target.
+        expense_groups: Dictionary of expense groups.
+    Returns:
+        Data for model training.
+    """
     model_data = _features(df)
     model_data["label"] = _labels(df, expense_groups)
     model_data["category_expense"] = df["category_expense"]
     return model_data
+
+
+def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
+    """Splits data into features and targets training and test sets.
+
+    Args:
+        data: Data containing features and target.
+        parameters: Parameters defined in parameters/data_science.yml.
+    Returns:
+        Split data.
+    """
+    X = data[parameters["features"]]
+    y = data[parameters["label"]]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=parameters["test_size"],
+        random_state=parameters["random_state"],
+        stratify=y,
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def train_model(
+    X_train: pd.DataFrame, y_train: pd.Series
+) -> HistGradientBoostingClassifier:
+    """Trains the classifier model.
+
+    Args:
+        X_train: Training data of independent features.
+        y_train: Training data for price.
+
+    Returns:
+        Trained model.
+    """
+    clf = HistGradientBoostingClassifier().fit(X_train, y_train)
+    return clf
+
+
+def evaluate_model(
+    classifier: HistGradientBoostingClassifier, X_test: pd.DataFrame, y_test: pd.Series
+) -> Dict[str, float]:
+    """Calculates and logs the most popular classification metrics.
+
+    Args:
+        classifier: Trained model.
+        X_test: Testing data of independent features.
+        y_test: Testing data for price.
+    """
+    logger = logging.getLogger(__name__)
+    y_pred = classifier.predict(X_test)
+
+    report_txt = classification_report(y_test, y_pred)
+    logger.info(report_txt)
+    report = classification_report(y_test, y_pred, output_dict=True)
+
+    metrics = pd.json_normalize(report, sep="_")
+    metrics = metrics.to_dict(orient="records")[0]
+    logger.info(metrics)
+    return metrics
--- src/brazilian_credit_card_spending/pipelines/ds_process/pipeline.py
+++ src/brazilian_credit_card_spending/pipelines/ds_process/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.19.3
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import get_model_data
+from .nodes import evaluate_model, get_model_data, split_data, train_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -15,6 +15,24 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=get_model_data,
                 inputs=["prim_mibolsillo", "params:expense_groups"],
                 outputs="model_data",
-            )
+            ),
+            node(
+                func=split_data,
+                inputs=["model_data", "params:model_options"],
+                outputs=["X_train", "X_test", "y_train", "y_test"],
+                name="split_data_node",
+            ),
+            node(
+                func=train_model,
+                inputs=["X_train", "y_train"],
+                outputs="classifier",
+                name="train_model_node",
+            ),
+            node(
+                func=evaluate_model,
+                inputs=["classifier", "X_test", "y_test"],
+                name="evaluate_model_node",
+                outputs="metrics",
+            ),
         ]
     )
